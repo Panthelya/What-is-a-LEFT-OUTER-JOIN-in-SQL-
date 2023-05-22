125653d8221c63094e3e69a992be8daab11068cd
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-05-22 20:19:24 +0200

    Add files via upload

---
+++ employees_DB.sql
@@ -0,0 +1,22 @@
+CREATE TABLE employees (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	department_id INT
+);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (1, 'Della', 'Hinchshaw', 'dhinchshaw0@virginia.edu', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (2, 'Roana', 'Andraud', 'randraud1@unblog.fr', 2);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (3, 'Nettle', 'Drewell', 'ndrewell2@timesonline.co.uk', 3);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (4, 'Coralie', 'Landre', 'clandre3@odnoklassniki.ru', 3);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (5, 'Frederica', 'Ketchaside', 'fketchaside4@wsj.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (6, 'Fenelia', 'Guisot', 'fguisot5@shop-pro.jp', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (7, 'Marysa', 'Portch', 'mportch6@xrea.com', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (8, 'Harlen', 'Drakard', 'hdrakard7@reuters.com', 2);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (9, 'Tiffie', 'Hauch', 'thauch8@t-online.de', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (10, 'Lura', 'Gravells', 'lgravells9@mapquest.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (11, 'Fae', 'Lagden', 'flagdena@nytimes.com', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (12, 'Chucho', 'Bear', 'cbearb@samsung.com', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (13, 'Tracie', 'Bellison', 'tbellisonc@nyu.edu', 2);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (14, 'Charita', 'Missen', 'cmissend@globo.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (15, 'Bear', 'Shoulder', 'bshouldere@intel.com', 1);
