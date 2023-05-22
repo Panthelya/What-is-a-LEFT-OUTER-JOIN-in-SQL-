bcc64e18ad02af1a17ad4d22b9a2695a3a044d8a
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-05-22 20:23:38 +0200

    Update employees_DB.sql

--- employees_DB.sql
+++ employees_DB.sql
@@ -5,18 +5,18 @@ CREATE TABLE employees (
 	email VARCHAR(50),
 	department_id INT
 );
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (1, 'Della', 'Hinchshaw', 'dhinchshaw0@virginia.edu', 1);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (2, 'Roana', 'Andraud', 'randraud1@unblog.fr', 2);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (3, 'Nettle', 'Drewell', 'ndrewell2@timesonline.co.uk', 3);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (4, 'Coralie', 'Landre', 'clandre3@odnoklassniki.ru', 3);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (5, 'Frederica', 'Ketchaside', 'fketchaside4@wsj.com', 1);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (6, 'Fenelia', 'Guisot', 'fguisot5@shop-pro.jp', 1);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (7, 'Marysa', 'Portch', 'mportch6@xrea.com', 4);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (8, 'Harlen', 'Drakard', 'hdrakard7@reuters.com', 2);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (9, 'Tiffie', 'Hauch', 'thauch8@t-online.de', 4);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (10, 'Lura', 'Gravells', 'lgravells9@mapquest.com', 1);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (11, 'Fae', 'Lagden', 'flagdena@nytimes.com', 4);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (12, 'Chucho', 'Bear', 'cbearb@samsung.com', 4);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (13, 'Tracie', 'Bellison', 'tbellisonc@nyu.edu', 2);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (14, 'Charita', 'Missen', 'cmissend@globo.com', 1);
-INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (15, 'Bear', 'Shoulder', 'bshouldere@intel.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (1, 'Della', 'Hinchshaw', 'dhinchshaw@company.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (2, 'Roana', 'Andraud', 'randraud@company.com', 2);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (3, 'Nettle', 'Drewell', 'ndrewell@company.com', 3);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (4, 'Coralie', 'Landre', 'clandre@company.com', 3);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (5, 'Frederica', 'Ketchaside', 'fketchaside@company.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (6, 'Fenelia', 'Guisot', 'fguisot@company.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (7, 'Marysa', 'Portch', 'mportch@company.com', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (8, 'Harlen', 'Drakard', 'hdrakard@company.com', 2);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (9, 'Tiffie', 'Hauch', 'thauch@company.com', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (10, 'Lura', 'Gravells', 'lgravells@company.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (11, 'Fae', 'Lagden', 'flagden@company.com', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (12, 'Chucho', 'Bear', 'cbear@company.com', 4);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (13, 'Tracie', 'Bellison', 'tbellison@company.com', 2);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (14, 'Charita', 'Missen', 'cmissen@company.com', 1);
+INSERT INTO employees (id, first_name, last_name, email, department_id) VALUES (15, 'Bear', 'Shoulder', 'bshoulder@company.com', 1);
