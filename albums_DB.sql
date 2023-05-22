ad5058e0bb760cc730d069c2ac5a4a25127e36f2
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-05-22 21:48:30 +0200

    Add files via upload

---
+++ albums_DB.sql
@@ -0,0 +1,20 @@
+CREATE TABLE albums  (
+	id INT,
+	album_title VARCHAR(50),
+	year_released INT,
+	artist_id INT,
+	grammy_category_id INT
+	
+);
+
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (1, 'Caribou', 1974, NULL, 2);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (2, 'Still Crazy After All These Years', 1975, 2, 1);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (3, 'Fulfillingness'' First Finale', 1974, 3, 1);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (4, 'Stranger to Stranger', 2016, 2, NULL);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (5, 'The Wall', 1979, NULL, 2);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (6, 'Songs in the Key of Life', 1976, 3, 1);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (7, 'Black Moses', 1971, 1, NULL);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (8, 'Innervisions', 1974, 3, 1);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (9, 'Shaft', 1971, 1, 2);
+INSERT INTO albums (id, album_title, year_released, artist_id, grammy_category_id) VALUES (10, 'Let''s Dance', 1983, NULL, 2);
+
