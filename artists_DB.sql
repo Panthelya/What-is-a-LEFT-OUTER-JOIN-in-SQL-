6324cf9078f543bbda52823837c05cf45140ff6b
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-05-22 21:46:38 +0200

    Add files via upload

---
+++ artists_DB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE artists (
+	id INT,
+	artist_name VARCHAR(50)
+	
+);
+
+INSERT INTO artists (id, artist_name) VALUES (1, 'Isaac Hayes');
+INSERT INTO artists (id, artist_name) VALUES (2, 'Paul Simon');
+INSERT INTO artists (id, artist_name) VALUES (3, 'Stevie Wonder');
