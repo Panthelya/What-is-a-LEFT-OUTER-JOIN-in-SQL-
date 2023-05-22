e1ebc4079d68d46f4f9dbd212653241f8acecd20
Panthelya <116551755+Panthelya@users.noreply.github.com>
2023-05-22 21:53:36 +0200

    Add files via upload

---
+++ grammy_award_DB.sql
@@ -0,0 +1,8 @@
+CREATE TABLE grammy_award (
+	id INT,
+	grammy_category VARCHAR(50)
+	
+);
+
+INSERT INTO grammy_award (id, grammy_category) VALUES (1, 'Album of the Year Winner');
+INSERT INTO grammy_award (id, grammy_category) VALUES (2, 'Album of the Year Nominee');
